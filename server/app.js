6a258e73b66ae4bbff96d6af8d15ce96fc0d9411
Paul I Wilson <paul.i.wilson@ft.com>
2015-04-20 12:09:09 +0100

    more logging

--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
 'use strict';
 var path = require('path');
+var debug = require('debug')('Great-Place-To-Meet');
 
 var express = require('express');
 var app = express();
 
-console.log(__dirname);
+debug("__dirname %s", __dirname);
 
 
 app.use('/public', express.static(path.resolve(__dirname + '/../public/')));
 app.use('/data', express.static(path.resolve(__dirname + '/../data/')));
 
 app.get('/*', function(req, res){
+	debug('Received request for %s', req.url);
 	res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
@@ -18,5 +20,5 @@ app.get('/*', function(req, res){
 var port = process.env.PORT || 3333;
 app.listen(port);
 
-console.log("listening on %s", port);
+debug("listening on %s", port);
 
