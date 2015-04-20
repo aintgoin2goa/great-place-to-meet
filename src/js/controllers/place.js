887adccddd4f3be42dfbcd25cd30910e95c011c7
Paul I Wilson <paul.i.wilson@ft.com>
2015-04-20 16:28:34 +0100

    Adding ilusration

--- src/js/controllers/place.js
+++ src/js/controllers/place.js
@@ -3,7 +3,20 @@ var data = require('../data');
 var render = require('../render');
 var $ = require('jquery');
 
+var illustrations = [
+	'public/images/illustrations/ill1-cup.svg',
+	'public/images/illustrations/ill2-teacup.svg'
+];
+
 var $container;
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getIllustration(){
+	return illustrations[getRandomInt(0, illustrations.length-1)];
+}
 
 function place(name){
 	if(!$container){
@@ -13,7 +26,9 @@ function place(name){
 	data.load(name).then(function(placeData){
 		render('place-template', $container, placeData);
 		$('#splash').removeClass('visible');
+		var ill = getIllustration();
 		$('#place').addClass('visible');
+		$('.illustration').css('background-image', 'url(' + ill + ')');
 	});
 }
 
