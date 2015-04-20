cb3d8e6014e09c605a5f86554a59e1c21e045c7c
Paul I Wilson <paul.i.wilson@ft.com>
2015-04-20 21:39:58 +0100

    maybe fixing static assets

--- server/app.js
+++ server/app.js
@@ -5,19 +5,11 @@ var debug = require('debug')('Great-Place-To-Meet');
 var express = require('express');
 var app = express();
 
-debug("__dirname %s", __dirname);
-debug("Resolved path to public directory %s", path.resolve(__dirname + '/../public/'));
 
-
-app.use('/public', express.static(path.resolve(__dirname + '/../public/')));
-app.use('/data', express.static(path.resolve(__dirname + '/../data/')));
+app.use(express.static('public'));
+app.use(express.static('data'));
 
 app.get('/*', function(req, res){
-	if(req.url.indexOf('public') > -1){
-		res.sendFile(path.resolve(__dirname, req.url));
-		return;
-	}
-
 	res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
