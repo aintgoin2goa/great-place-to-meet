c8a425776a07d08ec8b9e00db5cc644c837931a9
Paul I Wilson <paul.i.wilson@ft.com>
2015-04-20 21:58:35 +0100

    ffs

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(express.static(cwd + '/public'));
 app.use('/data', express.static(cwd + '/data'));
 
 app.get('/*', function(req, res){
+
+	console.log('HERE %s', req.url);
 	if(req.url.indexOf('data') > -1 || req.url.indexOf('public') > -1){
 		res.sendFile(path.resolve('.' + req.url));
 	}
